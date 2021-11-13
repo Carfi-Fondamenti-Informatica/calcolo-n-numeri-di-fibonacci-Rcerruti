8ff20dc029ea6faa455bbdf9eddb8f14cb5a4fdc
Rcerruti <91542393+Rcerruti@users.noreply.github.com>
2021-11-13 19:00:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0;
+    cin>>n;
+    if(n>=2){
+        int num1=1;
+        int num2=1;
+        int num3;
+        cout<<num1<<endl<<num2<<endl;
+        for(int i=2;i<n;i++){
+            num3=num1+num2;
+            num1=num2;
+            num2=num3;
+            cout<<num3<<endl;
+        }
+    }else{
+        cout<<"errore";
+    }
+    return 0;
 }
